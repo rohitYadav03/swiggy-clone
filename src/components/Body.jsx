eed60be0d90b5ce2cccc0fb14983ca028dbb72ab
Rohit Yadav <rohityadav85801@gmail.com>
2025-03-13 11:02:36 +0000

    filter logic added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,23 +1,38 @@
 import ResturantCard from "./ResturantCard"
 import resList from "../utils/mockData"
+import {useState} from "react"
 
 const Body = () => {
-    
+    const [listOfResturant, setListOfResturant] = useState(resList)
   return (
       <div className="body">
   
   <div className="serch-box">
-    <input type="text"></input>
-    <button className="btn-serchh">Search</button>
+
+  <button
+   className="btn-filter" 
+    onClick={() => {
+      let filterRes = listOfResturant.filter(
+        (curRes) => curRes.info.avgRating > 4.5
+      );
+      setListOfResturant(filterRes)
+    }}
+ 
+   >
+ Filter Resturant</button>
+    
   </div>
   
   <div className="resturant-container">
-     {resList.map((curRes) => 
-    <ResturantCard resData={curRes}/>
+     {listOfResturant.map((curRes) => 
+    <ResturantCard key={curRes.info.id} resData={curRes}/>
     )}
   </div>
+
       </div>
     )
+ 
+ 
   }
 
 export default Body;
